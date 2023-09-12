b7f72f35fdbbf0442047673d00d40e638e20a4c6
sarah <sarahsaray4@gmail.com>
2023-09-12 13:09:03 -0700

    cb

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet(void);
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+
+return (0);
+}

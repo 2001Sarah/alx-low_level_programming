6239286b1d7d170bfbd2505451eeeb9396c57706
sarah <sarahsaray4@gmail.com>
2023-10-19 14:40:23 -0700

    c1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_beat - prints "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+ *
+ * Return: void
+ */
+void print_beat(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/101-main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+int main()
+{
+printf("Hello, Holberton\n");
+return 0;
+}
